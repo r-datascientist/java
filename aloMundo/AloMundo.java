cc131c792f98440811644edbdb2979084bf804b9
R <202007012011@alunos.estacio.br>
2022-06-26 01:31:28 -0300

    primeiro commit

---
+++ aloMundo/AloMundo.java
@@ -0,0 +1,9 @@
+
+//TODO ARQUIVO JAVA É UMA CLASSE
+//SENDO ASSIM TODA CLASSE DEVE TER O MESMO NOME DO ARQUIVO
+public class AloMundo{
+    public static void main(String[] args){
+        //IMPRIMINDO MENSAGEM 
+        System.out.print("Alo Mundo!");
+    }
+}
